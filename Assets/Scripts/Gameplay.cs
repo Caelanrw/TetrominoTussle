206a801e42222fd529a9fcff2062147051b777f8
clnmcdowell <clnmcdowell@gmail.com>
2022-11-28 15:06:43 -0500

    Prototype fast drop added

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Gameplay : MonoBehaviour
 {
+    public GameObject[,] setBlocks = new GameObject[10,40]; //2 dimensional array to store set block location
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 20; //sets game frame rate to control drop speed
     }
 
     // Update is called once per frame
--- Assets/Scripts/Player1Blocks.cs
+++ Assets/Scripts/Player1Blocks.cs
@@ -4,35 +4,72 @@ using UnityEngine;
 
 public class Player1Blocks : MonoBehaviour
 {
-    public GameObject tetromino;
+    public GameObject tetromino; //parent tetromino game object
+    public bool active;
+    public bool droppable;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        active = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(active) //only allows movement for active tetromino
         {
-            tetromino.transform.position += new Vector3(0, 1, 0);
-        }
-        if(Input.GetKeyDown(KeyCode.S))
-        {
-            tetromino.transform.position += new Vector3(0, -1, 0);
-        }
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            tetromino.transform.eulerAngles -= new Vector3(0,0, 90);
+            //movement
+            if(Input.GetKeyDown(KeyCode.W)) //move tetromino up
+            {
+                tetromino.transform.position += new Vector3(0, 1, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.S)) //move tetromino down
+            {
+                tetromino.transform.position += new Vector3(0, -1, 0);
+            }
+
+            //rotation
+            if(Input.GetKeyDown(KeyCode.A)) //rotate tetromino 90 degrees
+            {
+                tetromino.transform.eulerAngles -= new Vector3(0,0, 90);
+            }
+
+            //dropping
+            
+            if(Input.GetKey(KeyCode.D)) //fast drop
+            {
+                droppable = DropCheck();
+                
+                if(droppable)
+                {
+                    tetromino.transform.position += new Vector3(1, 0, 0);
+                }
+                else
+                {
+                    active = false;
+                }
+            }
         }
     }
-    void DestroyChildren()
+
+    bool DropCheck()
+    {
+        if(tetromino.transform.position.x != 20)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+   /* void DestroyChildren() //Not functional just reference/test code
     {
         foreach(Transform block in tetromino.transform)
         {
             Destroy(block.gameObject);
         }
-    }
+    }*/
 }
