5af7ac11af52936a2f96b84244d032f8596576d7
clnmcdowell <clnmcdowell@gmail.com>
2022-11-28 23:50:11 -0500

    Added function to store inactive blocks

--- Assets/Scripts/Gameplay.cs
+++ Assets/Scripts/Gameplay.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Gameplay : MonoBehaviour
 {
-    public GameObject[,] setBlocks = new GameObject[10,40]; //2 dimensional array to store set block location
+    public static Transform[,] blocks; //2 dimensional array to store set block location
+    public GameObject I, J, L, O, S, T, Z; //tetromino prefabs
 
-    // Start is called before the first frame update
     void Start()
     {
         Application.targetFrameRate = 20; //sets game frame rate to control drop speed
+        SpawnTetromino();
+        blocks = new Transform[10,40];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnTetromino()
+    {
+        Instantiate(I, new Vector3(0, 5, 0), Quaternion.identity); //spawn 1 tetromino at point (0,5)
     }
 }
